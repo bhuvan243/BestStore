b4747e308db23e661971b10dae3949f8db08591c
bhuvan243 <bhuvan243@gmail.com>
2024-06-09 13:05:16 +0530

    project done

--- src/main/java/com/bhuvan/beststore/controllers/ProductsController.java
+++ src/main/java/com/bhuvan/beststore/controllers/ProductsController.java
@@ -124,6 +124,85 @@ public class ProductsController {
 	}
 	
 	// 47:37 - update products (post)
+	@PostMapping("/edit")
+	public ModelAndView updateProduct(@RequestParam int id, @Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+		ModelAndView mv = new ModelAndView("redirect:/products");
+		ModelAndView mvEdit = new ModelAndView("products/EditProduct");
+		
+		try {
+			Product product = repo.findById(id).get();
+			mv.addObject("product", product);
+			
+			if(result.hasErrors()) {
+				return mvEdit;
+			}
+			
+			// check if new image is there(i.e, updated)
+			if (!productDto.getImageFile().isEmpty()) {
+				// delete old image
+				String uploadDir = "public/images/";
+				Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+				
+				try {
+					Files.delete(oldImagePath);
+				} catch (Exception ex) {
+					System.out.println("Exception: " + ex.getMessage());
+				}
+				
+				// save the new image
+				MultipartFile image = productDto.getImageFile();
+				Date createdAt = new Date(2024, 4, 4);
+				String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+				
+				try(InputStream inputStream = image.getInputStream()) {
+					Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+				}
+				product.setImageFileName(storageFileName);
+			}
+			
+			product.setName(productDto.getName());
+			product.setBrand(productDto.getBrand());
+			product.setCategory(productDto.getCategory());
+			product.setPrice(productDto.getPrice());
+			product.setDescription(productDto.getDescription());
+			
+			repo.save(product);
+			
+		} catch(Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+		
+		
+		return mv;
+		
+	}
+	
+	//to delete a product
+	@GetMapping("/delete")
+	public ModelAndView deleteProduct(@RequestParam int id) {
+		ModelAndView mv = new ModelAndView("redirect:/products");
+		
+		try {
+			Product product = repo.findById(id).get();
+			
+			Path imagePath = Paths.get("public/images/" + product.getImageFileName());
+			//delete the product image - before deleting the obj from database
+			try {
+				Files.delete(imagePath);
+			} catch(Exception ex) {
+				System.out.println("Exception: " + ex.getMessage());
+			}
+			
+			//delete the product
+			repo.delete(product);
+			
+		} catch(Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+		
+		
+		return mv;
+	}
 	
 	
 }
